ef2065124d1de9ff47d0c49f5eb257b52fc32de4
unknown <badiquejoco@gmail.com>
2022-01-28 22:04:12 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import nextImage from '/public/next.svg';
@@ -5,6 +6,11 @@ import nextImage from '/public/next.svg';
 export default function Home() {
   return (
     <>
+
+ <Head>
+    <title>My Next.js Chatbot</title>
+ </Head>
+
     <section className="px-2 py-32 bg-white md:px-0">
   <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
     <div className="flex flex-wrap items-center sm:-mx-3">
