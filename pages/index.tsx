a0cc4855c2f665559f103a96bb64179a00844244
unknown <badiquejoco@gmail.com>
2022-01-28 22:00:48 +0800

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import nextImage from '/public/next.svg';
 
 export default function Home() {
@@ -13,21 +14,21 @@ export default function Home() {
             <span className="block xl:inline">Beautiful Pages to</span>
             <span className="block text-indigo-600 xl:inline">Tell Your Story!</span>
           </h1>
-          <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to build beautiful websites that convey your message and tell your story.</p>
+          <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It&apos;s never been easier to build beautiful websites that convey your message and tell your story.</p>
           <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-            <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+           <Link href="/"><a className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
               Try It Free
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-            </a>
-            <a href="#_" className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
+            </a></Link>
+            <Link href="/"><a className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
               Learn More
-            </a>
+            </a></Link>
           </div>
         </div>
       </div>
       <div className="w-full md:w-1/2">
          <div className="w-full h-auto overflow-hidden">
-           <Image src={nextImage}/>
+           <Image alt='Hero' src={nextImage}/>
          </div>
       </div>
     </div>
